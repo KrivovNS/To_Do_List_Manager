8d80159824ad46023142e75368aac3a2d84237a6
Nikita Krivov <niknike2007@mail.ru>
2026-03-05 17:59:15 +0300

    Реализован контроллер + тесты

---
+++ src/main/java/com/mipt/To_Do_List_Manager/model/TaskDto.java
@@ -0,0 +1,41 @@
+package com.mipt.To_Do_List_Manager.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ *  DTO для endpoint`s post и put.
+ */
+public class TaskDto {
+    @NotBlank(message = "title обязателен")
+    @JsonProperty("title")
+    private String title;
+
+    @NotBlank(message = "description обязателен")
+    @JsonProperty("description")
+    private String description;
+
+    public TaskDto() {
+    }
+
+    public TaskDto(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
