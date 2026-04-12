800450c02dd813ad8726d690ecbee9388a6bfc96
Nikita Krivov <niknike2007@mail.ru>
2026-04-12 04:16:55 +0300

    Поправлен маппер, добавлен плагин для аннотаций в pom.xml

--- src/main/java/com/mipt/To_Do_List_Manager/mapper/TaskMapper.java
+++ src/main/java/com/mipt/To_Do_List_Manager/mapper/TaskMapper.java
@@ -4,10 +4,14 @@ import com.mipt.To_Do_List_Manager.dto.TaskCreateDto;
 import com.mipt.To_Do_List_Manager.dto.TaskResponseDto;
 import com.mipt.To_Do_List_Manager.dto.TaskUpdateDto;
 import com.mipt.To_Do_List_Manager.model.Task;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.LinkedHashSet;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
@@ -15,7 +19,15 @@ public interface TaskMapper {
     Task toEntity(TaskCreateDto dto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "tags", ignore = true)
     void updateEntity(TaskUpdateDto dto, @MappingTarget Task task);
+
+    @AfterMapping
+    default void updateTags(TaskUpdateDto dto, @MappingTarget Task task) {
+        if (dto.tags() != null) {
+            task.setTags(new LinkedHashSet<>(dto.tags()));
+        }
+    }
 
     TaskResponseDto toResponseDto(Task task);
 }
